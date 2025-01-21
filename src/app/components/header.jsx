6be4c8807fe3361b82771ae7eed19c1c6d7c4b1a
Haikalkiuk <93031377+Haikalkiuk@users.noreply.github.com>
2025-01-22 03:58:36 +0700

    header.jsx

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -6,11 +6,11 @@ export default function Header() {
     return (
         <div className="flex gap-5 justify-center px-20 py-3.5 w-full shadow-sm bg-blue-950 max-md:flex-wrap max-md:px-5 max-md:max-w-full">
             <div className="flex gap-5 justify-between text-base font-bold text-center text-white whitespace-nowrap">
-               <button> <link rel="stylesheet" href="events/page" /><img
+               <button> <link rel="stylesheet" href="events/page" /></img
                     loading="lazy"
                     src="/images/logo-kuning.svg"
                     className="shrink-0 aspect-[1.82] w-[66px]" 
-                /><link/></button>
+                ></link></button>
                 <div className="flex gap-5 justify-between my-auto">
                    <button> <link rel="stylesheet" href="src/app/artispage/page.js" ><div className="justify-center" >Explore</div> <link/></button>
                     <button> <div className="justify-center">Partnership</div></button>
